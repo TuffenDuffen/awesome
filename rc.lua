3d099e28a5477c23af840b17604612ae2b189859
Tuffen Duffen <reg_i@wext.se>
2022-06-26 21:19:41 +0200

    removed awesome button and moved things

--- rc.lua
+++ rc.lua
@@ -201,19 +201,18 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
+            s.mylayoutbox,
             s.mytaglist,
             s.mypromptbox,
         },
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mybattery,
             myramusage,
             myswapusage,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
+            mybattery,
         },
     }
 end)
